9a1301d3999763de1dbbc2641eb2ad0308e5c68b
Thomas Bouldin <thomas.bouldin@gmail.com>
2020-06-17 12:40:31 -0700

    app includes opentelemetry; function runs

--- src/function.ts
+++ src/function.ts
@@ -4,14 +4,17 @@ import * as request from 'request-promise';
 let url: string;
 const mode = process.env['MODE'] || 'local';
 const port = process.env['PORT'] ? +process.env['PORT'] : 8080;
+const project = process.env['GOOGLE_CLOUD_PROJECT'] || 'fibonacci-tracing';
 switch (mode) {
   case 'local':
     url = `http://localhost:${port}`
     break;
   case 'gae':
-    url = `https://${process.env['GOOGLE_CLOUD_PROJECT']}.appspot.com`;
+    url = `https://${project}.appspot.com`;
     break;
-  case 'functions':
+  case 'gcf':
+    console.log(JSON.stringify(process.env, null, 2));
+    url = `https://us-central1-${project}.cloudfunctions.net/fibonacci`;
     break;
   default:
     throw new Error ("Unknown mode");
@@ -38,4 +41,19 @@ const func = async (req: express.Request, res: express.Response) => {
   }
 };
 
-export {url, port, func};
+const trampoline = async (req: express.Request, res: express.Response) => {
+  if (typeof req.query.n === 'undefined' || req.query.n === null) {
+    res.status(400).send('Missing query parameter "n"');
+    return;
+  }
+
+  const n = +req.query.n;
+  try {
+    const trampolined = await request.get(`${url}?n=${n}`);
+    res.status(200).send(trampolined);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+export {url, port, func, trampoline};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
-import * as Tracer from '@google-cloud/trace-agent';
+import { TraceExporter } from '@google-cloud/opentelemetry-cloud-trace-exporter';
+import {BatchSpanProcessor } from '@opentelemetry/tracing';
+import { NodeTracerProvider } from '@opentelemetry/node';
+
+const projectId = process.env['GOOGLE_PROJECT_ID'] || process.env['GOOGLE_CLOUD_PROJECT'];
+let batchProcessor: BatchSpanProcessor;
+if (projectId) {
+  console.log("Setting up stackdriver exporting for OpenTelemetry on project " + projectId);
+  // Use your existing provider
+  const provider = new NodeTracerProvider();
+  provider.register();
+
+  // Initialize the exporter
+  const exporter = new TraceExporter({projectId: projectId});
+
+  // Not a batch exporter to avoid a batch not flushing in time.
+  batchProcessor = new BatchSpanProcessor(exporter);
+  provider.addSpanProcessor(batchProcessor);
+}
+
 import express from 'express';
-import {func, url, port} from './function';
+import {func, trampoline, url, port} from './function';
+export {func, trampoline};
 
-Tracer.start();
-const app = express();
+if (require.main === module) {
+  const app = express();
 
-app.get('/', func);
+  app.get('/', func);
+  app.get('/flush', function(_, res) {
+    if (batchProcessor) {
+      batchProcessor.forceFlush();
+      res.send("Flushed");
+    } else {
+      res.send("Nothing ot flush");
+    }
+  });
 
-app.listen(port, () => {
-  console.log(`server started at ${url}`);
-});
-
+  app.listen(port, () => {
+    console.log(`server started at ${url}`);
+  });
+}
